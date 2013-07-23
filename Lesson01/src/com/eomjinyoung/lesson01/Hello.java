8a8d6b6f436f189cacc45e4f4bcfc0dc784d998d
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-07-23 09:28:14 +0900

    .

--- Lesson01/src/com/eomjinyoung/lesson01/Hello.java
+++ Lesson01/src/com/eomjinyoung/lesson01/Hello.java
@@ -3,7 +3,7 @@ package com.eomjinyoung.lesson01;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!2");
 
 	}
 
