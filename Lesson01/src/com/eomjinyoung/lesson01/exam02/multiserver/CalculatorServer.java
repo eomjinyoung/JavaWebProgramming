da604ba5d33af41027a9a2da925db22cd964031e
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-09-28 20:33:27 +0900

    add examples for multi-thread.

---
+++ Lesson01/src/com/eomjinyoung/lesson01/exam02/multiserver/CalculatorServer.java
@@ -0,0 +1,40 @@
+package com.eomjinyoung.lesson01.exam02.multiserver;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CalculatorServer {
+	private int port;
+	
+	public CalculatorServer(int port) {
+		this.port = port;
+	}
+	
+	public void service() throws Exception {
+		ServerSocket serverSocket = new ServerSocket(port);
+		System.out.println("CalculatorServer startup:");
+		
+		Socket socket = null;
+		
+		while(true) {
+			try {
+				System.out.println("waiting client...");
+				
+				socket = serverSocket.accept();
+				System.out.println("connected to client.");
+				
+				new CalculatorWorker(socket).start();
+				
+			} catch (Throwable e) {
+				System.out.println("connection error!");
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		CalculatorServer app = new CalculatorServer(8888);
+		app.service();
+	}
+}
+
