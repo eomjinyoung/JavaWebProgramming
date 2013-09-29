549d536e8aa2a8196f2e5bcc67232a085f8d453b
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-09-30 00:49:52 +0900

    .

--- Lesson01/src/com/eomjinyoung/lesson01/exam02/client/CalculatorAgent.java
+++ Lesson01/src/com/eomjinyoung/lesson01/exam02/client/CalculatorAgent.java
@@ -5,24 +5,18 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class CalculatorAgent {
-	String ip;
-	int port;
+	Socket socket = null;
+	PrintStream out = null;
+	Scanner in = null;
 	
-	public CalculatorAgent(String ip, int port) {
-		this.ip = ip;
-		this.port = port;
+	public CalculatorAgent(String ip, int port) throws Exception {
+		socket = new Socket(ip, port);
+		out = new PrintStream(socket.getOutputStream());
+		in = new Scanner(socket.getInputStream());
 	}
 	
 	public double compute(String operator, double a, double b) throws Exception {
-		Socket socket = null;
-		PrintStream out = null;
-		Scanner in = null;
-		
 		try {
-			socket = new Socket(ip, port);
-			out = new PrintStream(socket.getOutputStream());
-			in = new Scanner(socket.getInputStream());
-			
 			out.println(operator);
 			out.println(a);
 			out.println(b);
@@ -36,11 +30,17 @@ public class CalculatorAgent {
 			}
 		} catch (Exception e) {
 			throw e;
-			
-		} finally {
-			try {out.close();} catch(Exception e) {}
-			try {in.close();} catch(Exception e) {}
-			try {socket.close();} catch(Exception e) {}
-		}
+		} 
+	}
+	
+	public void close() {
+		try {
+			out.println("goodbye");
+			System.out.println(in.nextLine());
+		} catch (Exception e) {}
+		
+		try {out.close();} catch(Exception e) {}
+		try {in.close();} catch(Exception e) {}
+		try {socket.close();} catch(Exception e) {}
 	}
 }
