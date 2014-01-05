dcee14a4f5b070358b731ca721008527d0313bde
Jinyoung Eom <jinyoung.eom@gmail.com>
2014-01-05 22:55:37 +0900

    .

--- Lesson07Start/src/spms/bind/DataBinding.java
+++
@@ -1,5 +0,0 @@
-package spms.bind;
-
-public interface DataBinding {
-  Object[] getDataBinders();
-}
--- Lesson07Start/src/spms/controls/Controller.java
+++
@@ -1,7 +0,0 @@
-package spms.controls;
-
-import java.util.Map;
-
-public interface Controller {
-  String execute(Map<String, Object> model) throws Exception;
-}
