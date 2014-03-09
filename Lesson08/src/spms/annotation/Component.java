6e0d7061e8cc9bfff9f56431ea71f6eb136a2a27
Jinyoung Eom <jinyoung.eom@gmail.com>
2014-03-09 10:14:40 +0900

    .

--- Lesson08/src/spms/annotation/Component.java
+++
@@ -1,9 +0,0 @@
-package spms.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Component {
-  String value() default "";
-}
--- Lesson08/step01/WebContent/Tail.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<div style="background-color:#f0fff0;height:20px;padding:5px; margin-top:10px">
-SPMS &copy; 2013
-</div>
